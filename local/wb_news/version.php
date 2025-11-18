11d09f823b8c620661cbeafba9539a4673517d07
odpuentesg <odpuentesg@gmail.com>
2025-11-18 00:27:05 -0500

    metadata from plugin created

---
+++ local/wb_news/version.php
@@ -0,0 +1,23 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+/**
+ * Version metadata for the local_wb_news plugin.
+ *
+ * @package    local_wb_news
+ * @copyright  2025 GuiaMaestra
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->component = 'local_wb_news';
+$plugin->version   = 2025111800; // YYYYMMDDXX.
+$plugin->requires  = 2024052700; // Moodle 4.5.0 (or later).
+$plugin->maturity  = MATURITY_BETA;
+$plugin->release   = '1.0.0';
